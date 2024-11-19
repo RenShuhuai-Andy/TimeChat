3ef3d8d448500725a0b6e5c80b35c7f8b4419bb3
rrustlee <93597109+rrustlee@users.noreply.github.com>
2024-11-19 11:56:12 +0800

    Add files via upload

---
+++ benchmark/eval_videomme.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DIR="/home/wangziyue/workspace/TimeChat/ckpt/timechat"
+MODEL_DIR=${DIR}/timechat_7b.pth
+
+WS=32
+STRIDE=32
+
+TASK='videomme_subtitle'
+ANNO_DIR='/home/wangziyue/workspace/dataset/Video-MME'
+VIDEO_DIR='/home/wangziyue/workspace/dataset/Video-MME/data'
+SUBTITLE_DIR='/home/wangziyue/workspace/dataset/Video-MME/subtitle'
+
+NUM_FRAME=96
+OUTPUT_DIR=${DIR}/${TASK}
+
+python evaluate_vediomme.py  --video_path ${VIDEO_DIR} \
+--output_dir ${OUTPUT_DIR} --num_frames ${NUM_FRAME} \
+--subtitle_path ${SUBTITLE_DIR} \
+--timechat_model_path ${MODEL_DIR} \
+--window_size ${WS} --stride ${STRIDE} \
+--lora_alpha 20
