856f22d865d5f80f4f020ef07ef78196e15a2a6a
rrustlee <93597109+rrustlee@users.noreply.github.com>
2024-11-19 15:23:30 +0800

    Update eval_videomme.sh

--- benchmark/eval_videomme.sh
+++ benchmark/eval_videomme.sh
@@ -14,7 +14,7 @@ VIDEO_DIR='/home/wangziyue/workspace/dataset/Video-MME/data'
 NUM_FRAME=96
 OUTPUT_DIR=${DIR}/${TASK}
 
-python evaluate_vediomme.py  --video_path ${VIDEO_DIR} \
+python evaluate_videomme.py  --video_path ${VIDEO_DIR} \
 --output_dir ${OUTPUT_DIR} --num_frames ${NUM_FRAME} \
 --timechat_model_path ${MODEL_DIR} \
 --window_size ${WS} --stride ${STRIDE} \
